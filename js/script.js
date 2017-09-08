fc98a2fa915e01a94775a5615c270f33a60cd60e
Tobias Bernard <bertob@tu.computer>
2017-09-08 16:42:11 +0200

    [add] latin square

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+var LATIN_SQUARE = [[ 's50', 's20', 'c20', 'c250', 'p50', 'c50', 'p250', 'p20', 's250' ],
+                    [ 'p20', 'c250', 'p250', 's50', 'c20', 's20', 'c50', 's250', 'p50' ],
+                    [ 's250', 's50', 'c50', 'p20', 'p250', 'c250', 's20', 'p50', 'c20' ],
+                    [ 'c50', 'c20', 'p20', 'p250', 's50', 'p50', 's250', 's20', 'c250' ],
+                    [ 'c250', 'c50', 'p50', 's20', 's250', 'p250', 'c20', 's50', 'p20' ],
+                    [ 'p50', 'p20', 's20', 's250', 'c50', 's50', 'c250', 'c20', 'p250' ],
+                    [ 'p250', 'p50', 's50', 'c20', 'c250', 's250', 'p20', 'c50', 's20' ],
+                    [ 's20', 'p250', 's250', 'c50', 'p20', 'c20', 'p50', 'c250', 's50' ],
+                    [ 'c20', 's250', 'c250', 'p50', 's20', 'p20', 's50', 'p250', 'c50' ]];
+
+var LATIN_SQUARE_ROW = 0;
+
+var row = LATIN_SQUARE[LATIN_SQUARE_ROW];
+for (i=0; i<row.length; i++) {
+  console.log("3905349580340895345089", i, row[i]);
+  document.getElementById(row[i]).setAttribute("step", "number: " + (i+2));
+}
+
 
 document.getElementById("left-hand").addEventListener("trackpadup", function() {
   console.log("NEXT STEP");
---
+++ latinsquare-generator.js
@@ -0,0 +1,49 @@
+var elements = ["s20", "s50", "s250", "c20", "c50", "c250", "p20", "p50", "p250"];
+var makeRow = latinSquare(elements);
+
+for (i=0; i<25; i++) {
+  console.log(makeRow());
+}
+
+// Latin Square generator by Hugo Villeneuve https://github.com/hville/latin-square
+
+function latinSquare (row) {
+	var sN = row.length,
+			rowCount = 0
+
+	// prepare array of row and col indices for pre-sorting
+	var hSort = shuffle(sequence(sN)),
+			vSort = shuffle(hSort.slice())
+
+	return function nextRow (countORtarget) {
+		if (rowCount === sN) return countORtarget = null
+		var target = Array.isArray(countORtarget) ? countORtarget
+			: (countORtarget >= 0) ? Array(countORtarget)
+			: Array(sN)
+		if (target.length > sN) target.length = sN
+
+		for (var i = 0; i < target.length; ++i) {
+			var idx = hSort[i] + vSort[rowCount]
+			if (idx >= sN) idx -= sN
+			target[i] = row[idx]
+		}
+		rowCount++
+
+		return target
+	}
+}
+function sequence(n) {
+	for (var i = 0, a=Array(n); i < n; ++i) a[i] = i
+	return a
+}
+// modified from https://github.com/sindresorhus/array-shuffle
+function shuffle(arr) {
+	var len = arr.length
+	while (len) {
+		var rnd = Math.floor(Math.random() * len--)
+		var tmp = arr[len]
+		arr[len] = arr[rnd]
+		arr[rnd] = tmp
+	}
+	return arr
+}
