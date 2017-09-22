4918727fd89533387351e0e90d93e03031db9325
Tobias Bernard <bertob@tu.computer>
2017-09-22 17:52:40 +0200

    [feat] make tutorial interactive

--- js/step.js
+++ js/step.js
@@ -8,9 +8,34 @@ AFRAME.registerComponent("stepcontainer", {
     this.el.setAttribute("position", new THREE.Vector3(this.data.currentStep*(-STEP_DISTANCE), 0, -1));
 
     // scrolling tutorial
-    var icons = [23,66,143,22,11,55,12,231,67,46,144,89,31,219,178];
-    $(this.el).children("#tutorial-scroll").children("[card-stack]").get(0).components["card-stack"].setup(icons);
-    // this.el.children[1].children[0].components["card-stack"].setup(icons);
+    var icons = [227,66,185,214,11,55,220,231,67,46,144,239,47,32,61];
+    var testStack = $(this.el).children("#tutorial-scroll").children("[card-stack]").get(0);
+    testStack.components["card-stack"].setup(icons);
+
+    // clicking last icon on scroller moves to next step
+    var last = $(testStack).children().get(14);
+
+    // give elements time to be generated
+    setTimeout(
+      function() {
+        last.components.card.data.target = true;
+        last.addEventListener("mouseup", function() {
+          setTimeout(
+            function() {
+              document.getElementById("step-container").components.stepcontainer.next();
+            }, 1400);
+          });
+      }, 800);
+
+    // clicking selection test target moves to next step
+    document.getElementById("test-target").addEventListener("mouseup", function() {
+      setTimeout(
+        function() {
+          document.getElementById("step-container").components.stepcontainer.next();
+        }, 1400);
+    });
+
+    this.move(0);
   },
   next: function () {
     this.move(1);
@@ -22,12 +47,29 @@ AFRAME.registerComponent("stepcontainer", {
     this.data.currentStep += movement;
     console.log("MOVING TO STEP",this.data.currentStep);
 
+
+    if (this.data.currentStep === 0) {
+      var testTargets = [[87, 0], [0,0], [0,0], [0,0], [0,0]];
+      updateControllerIcons(testTargets, 0);
+      $("#selection-advice").get(0).setAttribute("visible", "true");
+      $("#scrolling-advice").get(0).setAttribute("visible", "false");
+    }
+
     // stop tutorial scroller from scrolling on other steps
     if (this.data.currentStep === 1) {
       $(this.el).children("#tutorial-scroll").get(0).components["step"].play();
+      console.log($("#controller-icons").get(0));
+      $("#controller-icons").get(0).setAttribute("visible", "false");
+      $("#selection-advice").get(0).setAttribute("visible", "false");
+      $("#scrolling-advice").get(0).setAttribute("visible", "true");
     }
     else {
       $(this.el).children("#tutorial-scroll").get(0).components["step"].pause();
+      $("#controller-icons").get(0).setAttribute("visible", "true");
+    }
+
+    if (this.data.currentStep === 2) {
+      $("#scrolling-advice").get(0).setAttribute("visible", "false");
     }
 
     // first experiment
