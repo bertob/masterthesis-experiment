6691d4803436a845e146a9747f649b08c8635ee3
Tobias Bernard <bertob@tu.computer>
2017-09-27 17:17:35 +0200

    [ui] add some info to tutorial popups

--- js/task-container.js
+++ js/task-container.js
@@ -16,30 +16,44 @@ AFRAME.registerComponent("taskcontainer", {
         // show first task instruciton popup
         var infoRect = document.createElement("a-box");
         infoRect.setAttribute("class", "first-task-popup");
-        infoRect.setAttribute("scale", "1.4 1.3 0.02");
-        infoRect.setAttribute("position", "0 1.23 0.5");
+        infoRect.setAttribute("scale", "1.4 1.5 0.02");
+        infoRect.setAttribute("position", "0 1.33 0.5");
         infoRect.setAttribute("material", "color: black; opacity: 0.8");
         $(".active-task").get(0).appendChild(infoRect);
 
         var title = document.createElement("a-entity");
         title.setAttribute("class", "first-task-popup");
-        title.setAttribute("position", "-0.3 1.5 0.54");
+        title.setAttribute("position", "-0.3 1.8 0.54");
         title.setAttribute("material", "color: white");
         title.setAttribute("text-geometry", "value: First Task; size: 0.1; height: 0.001;");
         $(".active-task").get(0).appendChild(title);
 
         var info1 = document.createElement("a-entity");
         info1.setAttribute("class", "first-task-popup");
-        info1.setAttribute("position", "-0.26 1.36 0.54");
+        info1.setAttribute("position", "-0.197 1.62 0.54");
         info1.setAttribute("material", "color: white");
-        info1.setAttribute("text-geometry", "value: This is the first task; size: 0.04; height: 0.001;");
+        info1.setAttribute("text-geometry", "value: Now the actual; size: 0.04; height: 0.001;");
         $(".active-task").get(0).appendChild(info1);
 
         var info2 = document.createElement("a-entity");
         info2.setAttribute("class", "first-task-popup");
-        info2.setAttribute("position", "-0.32 1.26 0.54");
+        info2.setAttribute("position", "-0.23 1.53 0.54");
         info2.setAttribute("material", "color: white");
-        info2.setAttribute("text-geometry", "value: of the actual experiment; size: 0.04; height: 0.001;");
+        info2.setAttribute("text-geometry", "value: experiment starts; size: 0.04; height: 0.001;");
+        $(".active-task").get(0).appendChild(info2);
+
+        var info1 = document.createElement("a-entity");
+        info1.setAttribute("class", "first-task-popup");
+        info1.setAttribute("position", "-0.365 1.36 0.54");
+        info1.setAttribute("material", "color: white");
+        info1.setAttribute("text-geometry", "value: Select the icons as quickly as; size: 0.04; height: 0.001;");
+        $(".active-task").get(0).appendChild(info1);
+
+        var info2 = document.createElement("a-entity");
+        info2.setAttribute("class", "first-task-popup");
+        info2.setAttribute("position", "-0.365 1.27 0.54");
+        info2.setAttribute("material", "color: white");
+        info2.setAttribute("text-geometry", "value: possible, while avoiding errors; size: 0.04; height: 0.001;");
         $(".active-task").get(0).appendChild(info2);
       }, 200);
 
